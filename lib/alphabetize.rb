08414d9620a47d6bc34f0197792b3946b62c1519
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 04:35:47 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@
 def alphabetize(arr)
   arr.sort_by do |word|
     word.split("").map do |character|
-      esperanto.alphabet.index(character)
+      esperanto_alphabet.index(character)
     end
   end
 end
