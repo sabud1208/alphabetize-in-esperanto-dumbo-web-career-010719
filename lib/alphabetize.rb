ba2e2d18b9074dba006010b715b3fa72034ea30c
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 04:37:54 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |word|
     word.split("").map do |character|
-    char=  alphabet.index(character)
+     alphabet.index(character)
     end
   end
   char
