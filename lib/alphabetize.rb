46ee3d8a6609c73387e44af152f2d7305b80c545
Stephanie Messad <sabud1208@gmail.com>
2019-01-02 04:31:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,13 @@
 
  require "pry"
 def alphabetize(expressions)
-  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", 
-    "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", 
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ",
+    "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s",
     "ŝ", "t", "u", "ŭ", "v", "z"]
   expressions.each do |a|
     a.chars.collect do |x|
       esperanto_alphabet.index(x)
+      binding.pry
   end
  end
 end
